ab69a2a709f911d554085f65fe83416723ffc18a
Picabea <iniakipicabea@gmail.com>
2024-08-14 19:30:57 +0200

    Cambios en mongoose

--- src/connection.js
+++ src/connection.js
@@ -4,7 +4,7 @@ const config = require('./config.js')
 module.exports = {
     async connectToDB(dbName) {
         await mongoose.connect(config.MONGO_URI, {
-            dbName
+            dbName: config.DBNAME
         })
     },
 
--- src/session/mongoStorage.js
+++ src/session/mongoStorage.js
@@ -2,9 +2,11 @@ const MongoStore = require('connect-mongo')
 const session = require('express-session')
 const defaultOptions = require('./defaultOptions')
 
+const config = require('../config')
+
 const storage = MongoStore.create({
-    dbName: 'ecommerce',
-    mongoUrl: 'mongodb://127.0.0.1:27017',
+    dbName: config.DBNAME,
+    mongoUrl: config.MONGO_URI,
     ttl: 120
 })
 
